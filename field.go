457b97f073ae6b74c7b415ced5b22f6055d24ae9
frankkang <frankkang@wesure.cn>
2021-11-29 15:46:19 +0800

    add

--- field.go
+++ field.go
@@ -2,9 +2,39 @@ package structs
 
 import (
 	"errors"
+	"reflect"
+	"fmt"
 )
 var (
 	errNotExported = errors.New("field is not exported")
 	errNotSettable = errors.New("field is not settable")
 )
 
+type Field struct {
+	value reflect.Value
+	field reflect.StructField
+	defaultTag string
+}
+
+func (f *Field) Tag(key string) string {
+	return f.field.Tag.Get(key)
+}
+
+func (f *Field) Value() interface{} {
+	return f.value.Interface()
+}
+
+func (f *Field) IsEmbedded() bool {
+	return f.field.Anonymous
+}
+
+func (f *Field) IsExported() bool {
+	return f.field.PkgPath == ""
+}
+
+func (f *Field) IsZero() bool {
+	zero := reflect.Zero(f.value.Type()).Interface()
+	cur := f.Value()
+
+	return
+}
